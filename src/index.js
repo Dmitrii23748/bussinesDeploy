17ad5609160d402a61780b341d2260f90662ef23
Dmitrii23748 <chunosov.dmitrij@bk.ru>
2022-05-02 18:16:37 +0300

    hash-router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import store from "./redux/store/store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -10,10 +10,10 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter hashType="noslash">
       <Provider store={store}>
         <App/>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
